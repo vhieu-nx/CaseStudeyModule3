f9206e6d66eabb6f5531642dc3496e8723de8fba
vhieu-nx <hieunvbhaf180070@fpt.edu.vn>
2021-06-01 23:51:23 +0700

    phân tầng first commit

---
+++ src/main/java/com/codegym/controller/admin/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.codegym.controller.admin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/HomeServlet")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/codegym/dao/IMovieDAO.java
@@ -0,0 +1,4 @@
+package com.codegym.dao;
+
+public class IMovieDAO {
+}
