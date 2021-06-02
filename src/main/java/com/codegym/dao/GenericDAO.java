c763103d1393379026ac61ec2427c94f49e704ef
vhieu-nx <hieunvbhaf180070@fpt.edu.vn>
2021-06-02 10:41:47 +0700

    add file function service, abstractmodel

--- src/main/java/com/codegym/dao/GenericDAO.java
+++ src/main/java/com/codegym/dao/GenericDAO.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface GenericDAO <T>{
     <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters);
     void update (String sql, Object... parameters);
-    Long insert (String sql, Object... parameters);
+    Integer insert (String sql, Object... parameters);
     int count(String sql, Object... parameters);
 }
--- src/main/java/com/codegym/dao/impl/AbtractDAO.java
+++ src/main/java/com/codegym/dao/impl/AbtractDAO.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 
 public class AbtractDAO<T> implements GenericDAO<T> {
     ResourceBundle resourceBundle = ResourceBundle.getBundle("db");
+
     public Connection getConnection() {
         try {
 //			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,7 +21,7 @@ public class AbtractDAO<T> implements GenericDAO<T> {
             String url = resourceBundle.getString("url");
             String user = resourceBundle.getString("user");
             String password = resourceBundle.getString("password");
-            return DriverManager.getConnection(url,user,password);
+            return DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException | SQLException e) {
             return null;
         }
@@ -82,16 +83,117 @@ public class AbtractDAO<T> implements GenericDAO<T> {
 
     @Override
     public void update(String sql, Object... parameters) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql);
+            setParameter(preparedStatement, parameters);
+            preparedStatement.executeUpdate();
+            connection.commit();
+
+        } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
 
     }
 
     @Override
-    public Long insert(String sql, Object... parameters) {
+    public Integer insert(String sql, Object... parameters) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            Integer id = null;
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            setParameter(preparedStatement, parameters);
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                id = resultSet.getInt(1);
+            }
+            connection.commit();
+            return id;
+        } catch (SQLException e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e2) {
+                e2.printStackTrace();
+            }
+        }
         return null;
+
     }
 
     @Override
     public int count(String sql, Object... parameters) {
-        return 0;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            int count = 0;
+            connection = getConnection();
+            statement = connection.prepareStatement(sql);
+            setParameter(statement, parameters);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+            return count;
+        } catch (SQLException e) {
+            return 0;
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                return 0;
+            }
+        }
     }
 }
