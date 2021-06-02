14d3e5e75d0e5d763e3f2bf020ec125bd5f6e4b5
deepvn98 <deepvn988@gmail.com>
2021-06-02 16:19:49 +0700

    add method remove, save,update

--- src/main/java/com/codegym/dao/impl/UserDAO.java
+++ src/main/java/com/codegym/dao/impl/UserDAO.java
@@ -1,11 +1,16 @@
 package com.codegym.dao.impl;
 
 import com.codegym.dao.IUserDAO;
+import com.codegym.dao.connection.ConnectionJDBC;
 import com.codegym.model.UserModel;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAO implements IUserDAO {
+    Connection connection = ConnectionJDBC.getConnection();
     @Override
     public List<UserModel> showAll() {
         return null;
@@ -13,17 +18,47 @@ public class UserDAO implements IUserDAO {
 
     @Override
     public void save(UserModel userModel) {
-
+        String sql = "insert into user(username, email, password, role) value ('?','?','?','?')";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,userModel.getName());
+            preparedStatement.setString(2,userModel.getEmail());
+            preparedStatement.setString(3,userModel.getPassword());
+            preparedStatement.setString(4,userModel.getRole());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void update(int id, UserModel userModel) {
+        String sql = "update user set username = ?,email = ?,password = ?,role = ? where user_id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,userModel.getName());
+            preparedStatement.setString(2,userModel.getEmail());
+            preparedStatement.setString(3,userModel.getPassword());
+            preparedStatement.setString(4,userModel.getRole());
+            preparedStatement.setInt(5,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
 
     }
 
     @Override
     public void delete(int id) {
-
+        String sql = "delete from user where user_id= ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
