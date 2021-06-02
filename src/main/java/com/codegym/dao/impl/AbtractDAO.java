9c9f0a1449155f3e9d527c245c46ea525399d9ea
vhieu-nx <hieunvbhaf180070@fpt.edu.vn>
2021-06-02 10:16:15 +0700

    create function chung result set and setParameter

--- src/main/java/com/codegym/dao/impl/AbtractDAO.java
+++ src/main/java/com/codegym/dao/impl/AbtractDAO.java
@@ -3,9 +3,8 @@ package com.codegym.dao.impl;
 import com.codegym.dao.GenericDAO;
 import com.codegym.mapper.RowMapper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -29,7 +28,56 @@ public class AbtractDAO<T> implements GenericDAO<T> {
 
     @Override
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        return null;
+        List<T> results = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement(sql);
+            setParameter(statement, parameters);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+            return results;
+        } catch (SQLException e) {
+            return null;
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                return null;
+            }
+        }
+    }
+
+    private void setParameter(PreparedStatement statement, Object[] parameters) {
+        try {
+            for (int i = 0; i < parameters.length; i++) {
+                Object parameter = parameters[i];
+                int index = i + 1;
+                if (parameter instanceof Long) {
+                    statement.setLong(index, (Long) parameter);
+                } else if (parameter instanceof String) {
+                    statement.setString(index, (String) parameter);
+                } else if (parameter instanceof Integer) {
+                    statement.setInt(index, (Integer) parameter);
+                } else if (parameter instanceof Timestamp) {
+                    statement.setTimestamp(index, (Timestamp) parameter);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
