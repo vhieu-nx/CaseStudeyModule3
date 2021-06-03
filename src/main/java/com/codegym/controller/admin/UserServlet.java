aaee6a38a4b60524cb5b6d2b83f45471908d8486
deepvn98 <deepvn988@gmail.com>
2021-06-03 14:45:46 +0700

    ok

--- src/main/java/com/codegym/controller/admin/UserServlet.java
+++ src/main/java/com/codegym/controller/admin/UserServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "UserServlet", value = "/UserServlet")
+@WebServlet(name = "UserServlet", value = "")
 public class UserServlet extends HttpServlet {
     UserService userService = new UserService();
 
